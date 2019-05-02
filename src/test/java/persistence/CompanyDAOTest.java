a0aa793befccd8dd9c66848c752049c96fc8c839
adrienED <adrien.chabas@gmail.com>
2019-05-02 13:45:33 +0200

    code review 02/05

--- src/test/java/persistence/CompanyDAOTest.java
+++ src/test/java/persistence/CompanyDAOTest.java
@@ -22,137 +22,94 @@ import model.Company;
 
 public class CompanyDAOTest {
 	ConnectionDAO connectionDAO = new ConnectionDAO();
-	
+
 	ResultSet resulSetMock = mock(ResultSet.class);
 
 	@Before
 	public void setUp() throws Exception {
-		
+
 		FileInputStream f;
 		try {
-			f = new FileInputStream("src/test/resources/db.properties");		
-				Properties properties = new Properties();
-	            properties.load(f);
-	          
-	            
+			f = new FileInputStream("src/test/resources/db.properties");
+			Properties properties = new Properties();
+			properties.load(f);
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@Test
 	public void testFindById() throws SQLException {
 		Company company = new Company();
-		
+
 		CompanyDAO companyDAO = CompanyDAO.getInstance();
 		company = companyDAO.findById(13L);
-		
+
 		Company companyRef = new Company();
 		companyRef.setId(13L);
 		companyRef.setName("IBM");
-		
+
 		assertEquals(companyRef, company);
-		
+
 	}
 
 	@Test
 	public void testPopulate() throws SQLException, IOException {
 
-		when (resulSetMock.getLong("id")).thenReturn(13L);
-		when (resulSetMock.getString("name")).thenReturn("IBM");
-		
+		when(resulSetMock.getLong("id")).thenReturn(13L);
+		when(resulSetMock.getString("name")).thenReturn("IBM");
+
 		CompanyDAO companyDAO = CompanyDAO.getInstance();
-		
+
 		Company companyRef = new Company();
 		companyRef.setId(13L);
 		companyRef.setName("IBM");
-		
-		assertEquals(companyRef, companyDAO.populate(resulSetMock));
-		
-	}
 
-	@Test
-	public void testGetAll() throws SQLException {
-		
-		 CompanyDAO companyDAO = CompanyDAO.getInstance();
-		 
-		 when(resulSetMock.getLong("id")).thenReturn(1L);
-			when(resulSetMock.getString("name")).thenReturn("Apple Inc.");
-			when(resulSetMock.getLong("id")).thenReturn(2L);
-			when(resulSetMock.getString("name")).thenReturn("Thinking Machines");
-			when(resulSetMock.getLong("id")).thenReturn(3L);
-			when(resulSetMock.getString("name")).thenReturn("RCA");
-			when(resulSetMock.getLong("id")).thenReturn(4L);
-			when(resulSetMock.getString("name")).thenReturn("Netronics");
-			when(resulSetMock.getLong("id")).thenReturn(5L);
-			when(resulSetMock.getString("name")).thenReturn("Tandy Corporation");
-			when(resulSetMock.getLong("id")).thenReturn(6L);
-			when(resulSetMock.getString("name")).thenReturn("COmmodore International");
-			when(resulSetMock.getLong("id")).thenReturn(7L);
-			when(resulSetMock.getString("name")).thenReturn("MOS Technology");
-			when(resulSetMock.getLong("id")).thenReturn(8L);
-			when(resulSetMock.getString("name")).thenReturn("Micro Instrumentation and Telemetry Systems");
-			when(resulSetMock.getLong("id")).thenReturn(9L);
-			when(resulSetMock.getString("name")).thenReturn("IMS Associates, Inc.");
-			when(resulSetMock.getLong("id")).thenReturn(10L);
-			when(resulSetMock.getString("name")).thenReturn("Digital Equipment Corporation");
-			when(resulSetMock.getLong("id")).thenReturn(11L);
-			when(resulSetMock.getString("name")).thenReturn("Lincoln Laboratory");
-			when(resulSetMock.getLong("id")).thenReturn(12L);
-			when(resulSetMock.getString("name")).thenReturn("Moore School of Electrical Engineering");
-			when(resulSetMock.getLong("id")).thenReturn(13L);
-			when(resulSetMock.getString("name")).thenReturn("IBM");
-			when(resulSetMock.getLong("id")).thenReturn(14L);
-			when(resulSetMock.getString("name")).thenReturn("Amiga Corporation");
-			when(resulSetMock.getLong("id")).thenReturn(15L);
-			when(resulSetMock.getString("name")).thenReturn("Canon");
-			when(resulSetMock.getLong("id")).thenReturn(16L);
-			when(resulSetMock.getString("name")).thenReturn("Nokia");
-			when(resulSetMock.getLong("id")).thenReturn(17L);
-			when(resulSetMock.getString("name")).thenReturn("Moore School of Electrical Engineering");
-			when(resulSetMock.getLong("id")).thenReturn(18L);
-			when(resulSetMock.getString("name")).thenReturn("OQO");
-			when(resulSetMock.getLong("id")).thenReturn(19L);
-			when(resulSetMock.getString("name")).thenReturn("NeXT");
-			when(resulSetMock.getLong("id")).thenReturn(20L);
-			when(resulSetMock.getString("name")).thenReturn("Atari");
-			
-			when(resulSetMock.next()).thenReturn(true).thenReturn(false);
-			
-			assertEquals(resulSetMock,companyDAO.getAll());
-		}
+		assertEquals(companyRef, companyDAO.populate(resulSetMock));
+
+	}
 
 	@Test
 	public void testGetAllIntInt() throws SQLException {
 		CompanyDAO companyDAO = CompanyDAO.getInstance();
-		 
-			when(resulSetMock.getLong("id")).thenReturn(1L);
-			when(resulSetMock.getString("name")).thenReturn("Apple Inc.");
-			when(resulSetMock.getLong("id")).thenReturn(2L);
-			when(resulSetMock.getString("name")).thenReturn("Thinking Machines");
-			when(resulSetMock.getLong("id")).thenReturn(3L);
-			when(resulSetMock.getString("name")).thenReturn("RCA");
-			when(resulSetMock.getLong("id")).thenReturn(4L);
-			when(resulSetMock.getString("name")).thenReturn("Netronics");
-			when(resulSetMock.getLong("id")).thenReturn(5L);
-			when(resulSetMock.getString("name")).thenReturn("Tandy Corporation");
-			when(resulSetMock.getLong("id")).thenReturn(6L);
-			when(resulSetMock.getString("name")).thenReturn("COmmodore International");
-			when(resulSetMock.getLong("id")).thenReturn(7L);
-			when(resulSetMock.getString("name")).thenReturn("MOS Technology");
-			when(resulSetMock.getLong("id")).thenReturn(8L);
-			when(resulSetMock.getString("name")).thenReturn("Micro Instrumentation and Telemetry Systems");
-			when(resulSetMock.getLong("id")).thenReturn(9L);
-			when(resulSetMock.getString("name")).thenReturn("IMS Associates, Inc.");
-			when(resulSetMock.getLong("id")).thenReturn(10L);
-			
-			
-			when(resulSetMock.next()).thenReturn(true).thenReturn(false);
-			
-			assertEquals(resulSetMock,companyDAO.getAll(10,10) );
-		}
+
+		List<Company> listCompanies = new ArrayList<Company>();
+
+		Company company = new Company();
+		company.setId(1L);
+		company.setName("Apple Inc.");
+		listCompanies.add(company);
+		company.setId(2L);
+		company.setName("Thinking Machines");
+		listCompanies.add(company);
+		company.setId(3L);
+		company.setName("RCA");
+		listCompanies.add(company);
+		company.setId(4L);
+		company.setName("Netronics");
+		listCompanies.add(company);
+		company.setId(5L);
+		company.setName("Tandy Corporation");
+		listCompanies.add(company);
+		company.setId(6L);
+		company.setName("COmmodore International");
+		listCompanies.add(company);
+		company.setId(7L);
+		company.setName("MOS Technology");
+		listCompanies.add(company);
+		company.setId(8L);
+		company.setName("Micro Instrumentation and Telemetry Systems");
+		listCompanies.add(company);
+		company.setId(9L);
+		company.setName("IMS Associates, Inc.");
+		listCompanies.add(company);
+		company.setId(10L);
+		company.setName("Digital Equipment Corporation");
+		listCompanies.add(company);
+
+		assertEquals(listCompanies, companyDAO.getAll(10, 1));
 	}
-
-
+}
