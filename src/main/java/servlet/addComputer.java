cc37cee867c17fa61e4cb383bf7ff75511ac27cc
adrienED <adrien.chabas@gmail.com>
2019-05-14 14:16:44 +0200

    backup avant refactor

--- src/main/java/servlet/addComputer.java
+++ src/main/java/servlet/addComputer.java
@@ -14,12 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.web.servlet.ViewResolver;
 
 import config.AppConfig;
 import dto.CompanyDTO;
@@ -38,19 +34,16 @@ public class addComputer extends HttpServlet {
 
 	static Logger logger = LoggerFactory.getLogger(addComputer.class);
 	
-	@Autowired
-	ViewResolver ViewResolver;
 	
-	@Autowired
-	ComputerValidator computerValidator;
-	@Autowired
-	ComputerMapper computerMapper;
-	@Autowired
-	ComputerService computerService;
-	
-
 	ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+	ComputerValidator computerValidator = (ComputerValidator) ctx.getBean("ComputerValidator");
+	
+	ComputerMapper computerMapper = (ComputerMapper) ctx.getBean("ComputerMapper");
+
 	CompanyService companyService = (CompanyService) ctx.getBean("CompanyService");
+
+	ComputerService computerService = (ComputerService) ctx.getBean("ComputerService");
 
 
 	public addComputer() {
